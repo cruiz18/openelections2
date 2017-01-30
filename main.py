7e4d62fb126ad88a9e0014054e693632774063d2
cruiz18 <scripteduser@Codes-MacBook-Air.local>
2017-01-30 17:50:42 -0500

    Init commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import webapp2
+import csv
+import String10
+import json
+
+def process_csv_data(csv_data):
+    precinct_to_votes= {}
+    is_header = True
+    f = StringIO.StringIO(csv_data)
+    rows = csv.reader(f, delimiter=',')
+    for row in rows:
+        if is_header:
+            is_header = False
+            continue
+        { "014/12": {
+            "county": "Queens",
+            "precinct": "014/23"
+            "votes": {
+            "Jill Stein / Ajamu Baraka": 10,
+            "Hillary Clinton / Tim Kaine": 373,
+            "Donald J. Trump / Michael R. Pence": 444,
+            "Gary Johnson / Bill Weld": 8 } },
+            "012/24": { … }, …
+        }
+    return precinct_to_votes
+
+class UploadHandler(webapp2.RequestHandler):
+    def post(self):
+        csv_data = self.request.POST.get('csv_file').file.read()
+        results = process_csv_data(csv_data)
+        for key in results:
+            self.response.out.write(json.dumps(results[key]) + "<br>" )
+
+#!/usr/bin/env python
+
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+class UploadHandler(webapp2.RequestHandler):
+    def post(self):
+        #csv_data= self.request.POST.get('csv_file').file.read()
+        self.response.out.write('hello world')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/upload', UploadHandler)
+], debug=True)
